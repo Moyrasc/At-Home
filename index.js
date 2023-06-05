e17e8074886ad063b54bf504a62af50019a83643
Moyrasc <noemisolac@gmail.com>
2023-06-05 20:08:27 +0200

    configuración bbdd

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 
 import express from 'express';
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js';
 
 //Crear app
-
 const app = express()
+
+//Conexión a la base de datos
+try {
+    await db.authenticate();
+    console.log('Conexión correcta a la BBDD')
+} catch (error) {
+    console.log(error)
+}
 
 //Habilitar PUG
 app.set('view engine', 'pug')
@@ -15,12 +23,11 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 //Routing
-
 app.use('/auth', usuarioRoutes)
 
 //Definir puerto y arrancar proyecto
-
 const port = 3000;
+
 app.listen(port, () => {
     console.log(`Servidor funcionando en puerto ${port}`)
 });
