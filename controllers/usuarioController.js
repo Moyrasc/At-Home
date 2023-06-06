11d546145421c5785ac5b9ea740feaeffc40d2d6
Moyrasc <noemisolac@gmail.com>
2023-06-06 12:02:11 +0200

    validación formulario registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
-import Usuario from '../models/Usuario.js'
+import { check, validationResult } from 'express-validator';
+import Usuario from '../models/Usuario.js';
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
@@ -12,6 +13,17 @@ const formularioRegistro = (req, res) => {
     })
 }
 const registrar = async (req, res) => {
+
+    //Validación
+    await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
+    await check('apellido').notEmpty().withMessage('El apellido es obligatorio').run(req)
+    await check('email').isEmail().withMessage('El email no es valido').run(req)
+    await check('password').isLength({ min: 8 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).withMessage('El password debe tener al menos 8 caracteres, una minúscula, una mayúscula y un caracte especial').run(req)
+    await check('confirmar_password').equals('password').withMessage('El password no coincide')
+
+    let resultado = validationResult(req)
+    res.json(resultado.array())
+
 
     const usuario = await Usuario.create(req.body)
     res.json(usuario)
