ca6942d3af8a485e3895b29fe05894536fb29bf7
Moyrasc <noemisolac@gmail.com>
2023-06-06 10:32:52 +0200

    instalación sequealize y creacion modelo usuario

---
+++ models/Usuario.js
@@ -0,0 +1,27 @@
+import { DataTypes } from 'sequelize';
+import db from '../config/db.js'
+
+const Usuario = db.define('usuarios', {
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    token: DataTypes.STRING,
+
+    confirmado: DataTypes.BOOLEAN
+
+})
+
+export default Usuario
