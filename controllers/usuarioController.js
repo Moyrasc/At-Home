71a12c0a47ef3c2f01970093a31c049adeb2be4e
Moyrasc <noemisolac@gmail.com>
2023-06-06 13:48:01 +0200

    encriptación contraseñas

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -13,23 +13,53 @@ const formularioRegistro = (req, res) => {
     })
 }
 const registrar = async (req, res) => {
-
     //Validación
     await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
     await check('apellido').notEmpty().withMessage('El apellido es obligatorio').run(req)
     await check('email').isEmail().withMessage('El email no es valido').run(req)
-    await check('password').isLength({ min: 8 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d@$.!%*#?&]/).withMessage('El password debe tener al menos 8 caracteres, una minúscula, una mayúscula y un caracte especial').run(req)
-    await check('confirmar_password').equals('password').withMessage('El password no coincide')
+    await check('password').isLength({ min: 8, max: 15 }).withMessage('El password debe tener entre 8 y 15 caracteres ').run(req)
+    await check('confirmar_password').equals(req.body.password).withMessage('El password no coincide').run(req)
 
     let resultado = validationResult(req)
-    res.json(resultado.array())
 
+    //Verificar resultado vacio
+    if (!resultado.isEmpty()) {
+        //Errores
+        return res.render('auth/registro', {
+            pagina: 'Crear cuenta',
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                email: req.body.email
+            }
+        })
+    }
+    // Extraer los datos
+    const { nombre, apellido, email, password } = req.body;
 
-    const usuario = await Usuario.create(req.body)
-    res.json(usuario)
-
+    //Verificar que el usuario no este duplicado
+    const existeUsuario = await Usuario.findOne({ where: { email } })
+    if (existeUsuario) {
+        return res.render('auth/registro', {
+            pagina: 'Crear cuenta',
+            errores: [{ msg: 'El usuario ya existe' }],
+            usuario: {
+                nombre: req.body.nombre,
+                apellido: req.body.apellido,
+                email: req.body.email
+            }
+        })
+    }
+    //Almacenar Usuario
+    await Usuario.create({
+        nombre,
+        apellido,
+        email,
+        password,
+        token: 123
+    })
 }
-
 const formularioRecuperarPassword = (req, res) => {
     res.render('auth/recuperar-password', {
         pagina: 'Recuperar acceso a tu cuenta'
