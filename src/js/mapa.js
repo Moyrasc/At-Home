b2f86802b6cf0ebd0fc72b415ae0c5c005d10cc3
Moyrasc <noemisolac@gmail.com>
2023-06-13 15:09:57 +0200

    añadido marcador al mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,11 +3,20 @@
     const lat = 37.6175227;
     const lng = -4.3275976;
     const mapa = L.map('mapa').setView([lat, lng], 15);
+    let marker;
 
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapa);
+
+    //Pin para la localizacion
+
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    })
+        .addTo(mapa)
 
 
 })()
