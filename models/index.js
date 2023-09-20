25564e870193a14fe01814a31bc82dc926bd9ac7
Moyrasc <noemisolac@gmail.com>
2023-09-20 14:14:07 +0200

    created message model and relationships

--- models/index.js
+++ models/index.js
@@ -2,15 +2,20 @@ import Propiedad from './Propiedad.js';
 import Categoria from "./Categoria.js";
 import Precio from "./Precio.js";
 import Usuario from "./Usuario.js"
+import Mensaje from './mensaje.js';
 
 // Precio.hasOne(Propiedad)
 Propiedad.belongsTo(Precio, { foreignKey: 'precioId' })
 Propiedad.belongsTo(Categoria, { foreignKey: 'categoriaId' })
 Propiedad.belongsTo(Usuario, { foreignKey: 'usuarioId' })
 
+Mensaje.belongsTo(Propiedad, { foreignKey:'propiedadId' })
+Mensaje.belongsTo(Usuario, { foreignKey:'usuarioId' })
+
 export {
     Propiedad,
     Categoria,
     Precio,
-    Usuario
+    Usuario,
+    Mensaje
 }
